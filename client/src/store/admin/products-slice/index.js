144f7bdb334431bb44196dc1f4364782f20c5fa9
yuvraj singh <97425208+yuvraj1016@users.noreply.github.com>
2024-11-23 16:05:55 +0530

    Add files via upload

---
+++ client/src/store/admin/products-slice/index.js
@@ -0,0 +1,85 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+export const addNewProduct = createAsyncThunk(
+  "/products/addnewproduct",
+  async (formData) => {
+    const result = await axios.post(
+      "https://ecommerce-oqyo.onrender.com/api/admin/products/add",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return result?.data;
+  }
+);
+
+export const fetchAllProducts = createAsyncThunk(
+  "/products/fetchAllProducts",
+  async () => {
+    const result = await axios.get(
+      "https://ecommerce-oqyo.onrender.com/api/admin/products/get"
+    );
+
+    return result?.data;
+  }
+);
+
+export const editProduct = createAsyncThunk(
+  "/products/editProduct",
+  async ({ id, formData }) => {
+    const result = await axios.put(
+      `https://ecommerce-oqyo.onrender.com/api/admin/products/edit/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return result?.data;
+  }
+);
+
+export const deleteProduct = createAsyncThunk(
+  "/products/deleteProduct",
+  async (id) => {
+    const result = await axios.delete(
+      `https://ecommerce-oqyo.onrender.com/api/admin/products/delete/${id}`
+    );
+
+    return result?.data;
+  }
+);
+
+const AdminProductsSlice = createSlice({
+  name: "adminProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
+});
+
+export default AdminProductsSlice.reducer;
